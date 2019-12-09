aed11c36976b48ff4e986cbea568de890adad258
LouisB80 <louisbernardh@gmail.com>
2019-12-09 12:16:45 +0100

    modif

--- Exercice1Partie1Php/index.php
+++ Exercice1Partie1Php/index.php
@@ -13,7 +13,7 @@
 <body>
   <h1>Exercice 1 Partie 1 PHP</h1>
   <div class="container">
-    <p><?php echo "$name";?></p>
+    <p><?php echo "$name"; //On peut aussi écrire echo de la manière suivante:  <?= $name ? > ?></p>
   </div>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
